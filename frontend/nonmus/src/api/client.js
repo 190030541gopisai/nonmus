ff7285bbb0f8681cc346cde79425f75d5aed3fb9
Gopi Sai Gottipati <gottipatigopisai@gmail.com>
2026-08-04 07:25:39 +0530

    remove infinite loop interceptor 401

--- frontend/nonmus/src/api/client.js
+++ frontend/nonmus/src/api/client.js
@@ -18,7 +18,8 @@ apiClient.interceptors.response.use(
 
       if (
           error.response?.status === 401 &&
-          !originalRequest._retry
+          !originalRequest._retry &&
+          !originalRequest.url.includes("/auth/refresh")
       ) {
         originalRequest._retry = true;
 
